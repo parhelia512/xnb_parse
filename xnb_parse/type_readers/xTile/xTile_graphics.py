8f926d4607e92188801328a380e9c56e3d5401c5
Alexander Lavrov <LavrovAS@gmail.com>
2016-03-08 11:23:04 +0300

    xTile reader

--- xnb_parse/type_readers/xTile/xTile_graphics.py
+++ xnb_parse/type_readers/xTile/xTile_graphics.py
@@ -4,9 +4,8 @@ xTile graphics type readers
 
 from __future__ import print_function
 
-from xnb_parse.type_reader import (TypeReaderPlugin, BaseTypeReader, ValueTypeReader, GenericTypeReader,
-                                   generic_reader_type)
-from xnb_parse.xna_types.xTile.xTile_graphics import (Map, TileSheet, Size, Layer, Tile)
+from xnb_parse.type_reader import (TypeReaderPlugin, BaseTypeReader)
+from xnb_parse.xna_types.xTile.xTile_graphics import *
 
 # avoiding circular import
 PLATFORM_WINDOWS = b'w'
@@ -23,123 +22,171 @@ class XtileMapReader(BaseTypeReader, TypeReaderPlugin):
             magic_word = self.stream.read(6)
             assert magic_word == b'tBIN10', 'Wrong magic word: {}'.format(magic_word)
 
-            id_ = self.stream.read_long_string()
-            description = self.stream.read_long_string()
-            properties = self.read_properties()
-            tilesheets = self.read_tilesheets()
-            layers = self.read_layers()
+            map_ = Map()
+            map_.id_ = self.stream.read_long_string()
+            map_.description = self.stream.read_long_string()
+            map_.properties = self.read_properties()
+            map_.tilesheets = self.read_tilesheets(map_)
+            map_.layers = self.read_layers(map_)
 
-            return Map(id_, description, properties, tilesheets, layers)
+            return map_
         else:
             raise NotImplementedError('Only windows support at this moment')
 
     def read_properties(self):
-        if self.file_platform == PLATFORM_WINDOWS:
-            property_count = self.stream.read_int32()
+        property_count = self.stream.read_int32()
 
-            result = {}
-            for i in range(property_count):
-                key = self.stream.read_long_string()
-                type_ = self.stream.read_7bit_encoded_int()
+        result = {}
+        for i in range(property_count):
+            key = self.stream.read_long_string()
+            type_ = self.stream.read_7bit_encoded_int()
 
-                if type_ == 0:  # bool
-                    value = self.stream.read_boolean()
-                elif type_ == 1:  # int
-                    value = self.stream.read_int32()
-                elif type_ == 2:  # float
-                    value = self.stream.read_single()
-                elif type_ == 3:  # string
-                    value = self.stream.read_long_string()
-                else:
-                    raise ValueError('Unexpected type')
+            if type_ == 0:  # bool
+                value = self.stream.read_boolean()
+            elif type_ == 1:  # int
+                value = self.stream.read_int32()
+            elif type_ == 2:  # float
+                value = self.stream.read_single()
+            elif type_ == 3:  # string
+                value = self.stream.read_long_string()
+            else:
+                raise ValueError('Unexpected type')
 
-                result[key] = value
+            result[key] = value
 
-            return result
-        else:
-            raise NotImplementedError('Only windows support at this moment')
+        return result
 
-    def read_tilesheets(self):
-        if self.file_platform == PLATFORM_WINDOWS:
-            tilesheet_count = self.stream.read_int32()
+    def read_tilesheets(self, map_):
+        tilesheet_count = self.stream.read_int32()
 
-            result = []
-            for i in range(tilesheet_count):
-                id_ = self.stream.read_long_string()
-                description = self.stream.read_long_string()
-                image_source = self.stream.read_long_string()
-                sheet_size = self.read_size()
-                tile_size = self.read_size()
-                margin = self.read_size()
-                spacing = self.read_size()
-                properties = self.read_properties()
+        result = []
+        for _ in range(tilesheet_count):
+            tilesheet = self.read_tilesheet(map_)
+            result.append(tilesheet)
 
-                tilesheet = TileSheet(id_, description, image_source, sheet_size, tile_size, margin, spacing, properties)
-                result.append(tilesheet)
+        return result
 
-            return result
-        else:
-            raise NotImplementedError('Only windows support at this moment')
+    def read_tilesheet(self, map_):
+        id_ = self.stream.read_long_string()
+        description = self.stream.read_long_string()
+        image_source = self.stream.read_long_string()
+        sheet_size = self.read_size()
+        tile_size = self.read_size()
+        margin = self.read_size()
+        spacing = self.read_size()
+        properties = self.read_properties()
+
+        tilesheet = TileSheet(id_, map_, image_source, sheet_size, tile_size)
+        tilesheet.description = description
+        tilesheet.margin = margin
+        tilesheet.spacing = spacing
+        tilesheet.properties = properties
+
+        return tilesheet
 
     def read_size(self):
-        if self.file_platform == PLATFORM_WINDOWS:
-            width = self.stream.read_int32()
-            height = self.stream.read_int32()
+        width = self.stream.read_int32()
+        height = self.stream.read_int32()
 
-            return Size(width, height)
-        else:
-            raise NotImplementedError('Only windows support at this moment')
+        return Size(width, height)
 
-    def read_layers(self):
-        if self.file_platform == PLATFORM_WINDOWS:
-            layers_count = self.stream.read_int32()
+    def read_layers(self, map_):
+        layers_count = self.stream.read_int32()
 
-            result = []
-            for i in range(layers_count):
-                id_ = self.stream.read_long_string()
-                visible = self.stream.read_boolean()
-                description = self.stream.read_long_string()
-                layer_size = self.read_size()
-                tile_size = self.read_size()
-                properties = self.read_properties()
-                tiles = self.read_tiles(layer_size)
-
-                tilesheet = Layer(id_, visible, description, layer_size, tile_size, properties, tiles)
-                result.append(tilesheet)
-
-            return result
-        else:
-            raise NotImplementedError('Only windows support at this moment')
-
-    def read_tiles(self, layer_size):
         result = []
-        for current_y in range(layer_size.height):
-            current_x = 0
-            while current_x < layer_size.width:
+        for _ in range(layers_count):
+            layer = self.read_layer(map_)
+            result.append(layer)
+
+        return result
+
+    def read_layer(self, map_):
+        id_ = self.stream.read_long_string()
+        visible = self.stream.read_boolean()
+        description = self.stream.read_long_string()
+        layer_size = self.read_size()
+        tile_size = self.read_size()
+
+        layer = Layer(id_, map_, layer_size, tile_size)
+        layer.description = description
+        layer.visible = visible
+        layer.properties = self.read_properties()
+        layer.tiles = self.read_tiles(layer)
+
+        return layer
+
+    def read_static_tile(self, layer, tilesheet):
+        tile_index = self.stream.read_int32()
+        blend_mode = BlendMode(self.stream.read_7bit_encoded_int())  # enum(Alpha, Additive)
+
+        static_tile = StaticTile(layer, tilesheet, blend_mode, tile_index)
+        static_tile.properties = self.read_properties()
+
+        return static_tile
+
+    def read_animated_tile(self, layer):
+        frame_interval = self.stream.read_int32()
+        tile_frame_count = self.stream.read_int32()
+        tile_frames = self.read_animation_tiles(layer, tile_frame_count)
+
+        animated_tile = AnimatedTile(layer, tile_frames, frame_interval)
+        animated_tile.properties = self.read_properties()
+
+        return animated_tile
+
+    def read_tiles(self, layer):
+        layer_size = layer.layer_size
+        result = [[None for x in range(layer_size.width)] for y in range(layer_size.height)]
+        tilesheet = None
+
+        for y in range(layer_size.height):
+            x = 0
+            while x < layer_size.width:
                 ch = self.stream.read_char()
                 if ch == 'T':  # tilesheet id
                     tilesheet_id = self.stream.read_long_string()
+                    tilesheet = layer.map_.get_tilesheet(tilesheet_id)
 
                 elif ch == 'N':  # nulls
                     null_count = self.stream.read_int32()
-                    current_x += null_count
+                    x += null_count
 
                 elif ch == 'S':  # static tile
-                    tile_index = self.stream.read_int32()
-                    blend_mode = self.stream.read_7bit_encoded_int()  # enum(Alpha, Additive)
-                    properties = self.read_properties()
-                    x = current_x
-                    y = current_y
+                    if tilesheet is None:
+                        raise ValueError('tilesheet')
 
-                    if tilesheet_id is None:
-                        raise ValueError('tilesheet_id')
-
-                    tile = Tile(tile_index, blend_mode, properties, x, y, tilesheet_id)
-
-                    result.append(tile)
-                    current_x += 1
+                    result[y][x] = self.read_static_tile(layer, tilesheet)
+                    x += 1
 
                 elif ch == 'A':  # animated tile
-                    raise NotImplementedError('')  # TODO: do not forget
+                    if tilesheet is None:
+                        raise ValueError('tilesheet')
+
+                    result[y][x] = self.read_animated_tile(layer)
+                    x += 1
+                else:
+                    raise ValueError('ch: {}'.format(ch))
+
+        return result
+
+    def read_animation_tiles(self, layer, tile_frame_count):
+        tilesheet = None
+        result = []
+        while tile_frame_count > 0:
+            ch = self.stream.read_char()
+            if ch == 'T':  # tilesheet id
+                tilesheet_id = self.stream.read_long_string()
+                tilesheet = layer.map_.get_tilesheet(tilesheet_id)
+
+            elif ch == 'S':  # static tile
+                if tilesheet is None:
+                    raise ValueError('tilesheet')
+
+                tile_frame = self.read_static_tile(layer, tilesheet)
+                result.append(tile_frame)
+
+                tile_frame_count -= 1
+            else:
+                raise ValueError('ch: {}'.format(ch))
 
         return result
--- xnb_parse/xna_types/xTile/xTile_graphics.py
+++ xnb_parse/xna_types/xTile/xTile_graphics.py
@@ -1,90 +1,187 @@
 """
-xTile graphics types
+xTile objects
 """
-
-from __future__ import print_function
-
-
-class Map(object):
-    def __init__(self, id_, description, properties, tilesheets, layers):
-        self.id_ = id_
-        self.description = description
-        self.properties = properties
-        self.tilesheets = tilesheets
-        self.layers = layers
-
-    def __str__(self):
-        return 'Map \'{}\' d:\'{}\' p:{} t:{} l:{}'.format(self.id_, self.description, self.properties,
-                                                           self.tilesheets, self.layers)
-
-    def __repr__(self):
-        return self.__str__()
+from enum import Enum
+from uuid import uuid4
 
 
-class TileSheet(object):
-    def __init__(self, id_, description, image_source, sheet_size, tile_size, margin, spacing, properties):
-        self.id_ = id_
-        self.description = description
-        self.image_source = image_source
-        self.sheet_size = sheet_size
-        self.tile_size = tile_size
-        self.margin = margin
-        self.spacing = spacing
-        self.properties = properties
-
-    def __str__(self):
-        return 'TileSheet \'{}\' d:\'{}\' i:\'{}\' s:{} t:{} m:{} s:{} p:{}'.format(self.id_, self.description,
-                                                                               self.image_source, self.sheet_size,
-                                                                               self.tile_size, self.margin,
-                                                                               self.spacing, self.properties)
-
-    def __repr__(self):
-        return self.__str__()
+class BlendMode(Enum):
+    Alpha = 0
+    Additive = 1
 
 
 class Size(object):
-    def __init__(self, width, height):
+    width = None
+    height = None
+
+    def __init__(self, width, height=None):
         self.width = width
-        self.height = height
+        self.height = height or width
 
     def __str__(self):
-        return 'Size w:{} h:{}'.format(self.width, self.height)
+        return '<Size: {}x{}>'.format(self.width, self.height)
 
     def __repr__(self):
         return self.__str__()
 
 
-class Layer(object):
-    def __init__(self, id_, visible, description, layer_size, tile_size, properties, tiles):
-        self.id_ = id_
-        self.visible = visible
-        self.description = description
+class Component(object):
+    id_ = None
+    properties = None
+
+    def __init__(self, id_=None):
+        self.id_ = id_ or uuid4().__str__()
+        self.properties = {}
+
+    def __str__(self):
+        return '<Component: \'{}\'>'.format(self.id_)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class DescribedComponent(Component):
+    description = None
+
+    def __init__(self, id_, description=None):
+        super(DescribedComponent, self).__init__(id_)
+        self.description = description or ''
+
+    def __str__(self):
+        return '<DescribedComponent: \'{}\', \'{}\'>'.format(self.id_, self.description)
+
+
+class TileSheet(DescribedComponent):
+    map_ = None
+    image_source = None
+    sheet_size = None
+    tile_size = None
+    margin = None
+    spacing = None
+
+    def __init__(self, id_, map_, image_source, sheet_size, tile_size):
+        super(TileSheet, self).__init__(id_, None)
+        self.map_ = map_
+        self.image_source = image_source
+        self.sheet_size = sheet_size
+        self.tile_size = tile_size
+        self.margin = self.spacing = Size(0, 0)
+
+    def __str__(self):
+        return '<TileSheet: \'{}\', \'{}\'>'.format(self.id_, self.description)
+
+
+class Tile(Component):
+    layer = None
+
+    def __init__(self, layer):
+        super(Tile, self).__init__()
+        self.layer = layer
+
+    @property
+    def blend_mode(self):
+        raise NotImplementedError()
+
+    @property
+    def tilesheet(self):
+        raise NotImplementedError()
+
+    @property
+    def tile_index(self):
+        raise NotImplementedError()
+
+    def __str__(self):
+        return '<Tile: \'{}\'>'.format(self.id_)
+
+
+class StaticTile(Tile):
+    def __init__(self, layer, tilesheet, blend_mode, tile_index):
+        super(StaticTile, self).__init__(layer)
+        self._blend_mode = blend_mode
+        self._tilesheet = tilesheet
+        self._tile_index = tile_index
+
+    @property
+    def blend_mode(self):
+        return self._blend_mode
+
+    @property
+    def tilesheet(self):
+        return self._tilesheet
+
+    @property
+    def tile_index(self):
+        return self._tile_index
+
+    def __str__(self):
+        return '<StaticTile: \'{}\'>'.format(self.id_)
+
+
+class AnimatedTile(Tile):
+    def __init__(self, layer, tile_frames, frame_interval):
+        super(AnimatedTile, self).__init__(layer)
+        self.tile_frames = tile_frames
+        self.frame_interval = frame_interval
+        self.animation_interval = frame_interval * len(tile_frames)
+
+    def _get_current_frame(self):
+        animation_time = self.layer.map_.elapsed_time % self.animation_interval
+        current_index = animation_time // self.frame_interval
+        return self.tile_frames[current_index]
+
+    @property
+    def blend_mode(self):
+        current_frame = self._get_current_frame()
+        return current_frame.blend_mode
+
+    @property
+    def tilesheet(self):
+        current_frame = self._get_current_frame()
+        return current_frame.tilesheet
+
+    @property
+    def tile_index(self):
+        current_frame = self._get_current_frame()
+        return current_frame.tile_index
+
+    def __str__(self):
+        return '<AnimatedTile: \'{}\'>'.format(self.id_)
+
+
+class Layer(DescribedComponent):
+    map_ = None
+    tilesheets = None
+    layer_size = None
+    tile_size = None
+    tiles = None
+    visible = False
+
+    def __init__(self, id_, map_, layer_size, tile_size):
+        super(Layer, self).__init__(id_)
+        self.map_ = map_
+        self.tilesheets = map_.tilesheets
         self.layer_size = layer_size
         self.tile_size = tile_size
-        self.properties = properties
-        self.tiles = tiles
+        self.tiles = [[None for x in range(layer_size.width)] for y in range(layer_size.height)]
+        self.visible = True
 
     def __str__(self):
-        return 'Layer \'{}\' v:{} d:\'{}\' ls:{} ts:{} p:{} t:{}'.format(self.id_, self.visible, self.description,
-                                                                         self.layer_size, self.tile_size,
-                                                                         self.properties, self.tiles)
-
-    def __repr__(self):
-        return self.__str__()
+        return '<Layer: \'{}\', \'{}\'>'.format(self.id_, self.description)
 
 
-class Tile(object):
-    def __init__(self, tile_index, blend_mode, properties, x, y, tilesheet_id):
-        self.tile_index = tile_index
-        self.blend_mode = blend_mode
-        self.properties = properties
-        self.x = x
-        self.y = y
-        self.tilesheet_id = tilesheet_id
+class Map(DescribedComponent):
+    tilesheets = None
+    layers = None
+    elapsed_time = None
+
+    def __init__(self, id_=None):
+        super(Map, self).__init__(id_ or 'Untitled map', None)
+        self.tilesheets = []
+        self.layers = []
+        self.elapsed_time = 0
+
+    def get_tilesheet(self, tilesheet_id):
+        return [tilesheet for tilesheet in self.tilesheets if tilesheet.id_ == tilesheet_id][0]
 
     def __str__(self):
-        return 'Tile i:{} b:{} p:{} x:{} y:{} t:{}'.format(self.tile_index, self.blend_mode, self.properties, self.x,
-                                                           self.y, self.tilesheet_id)
-
-    def __repr__(self):
-        return self.__str__()
+        return '<Map: \'{}\', \'{}\'>'.format(self.id_, self.description)
